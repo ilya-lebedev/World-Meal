04957f2cce82ddf5c66232b2e50dee0f39b1b046
Ilya Lebedev <ilya.al.lebedev@gmail.com>
2018-06-29 10:13:41 +0300

    feat: Add MealRecipeDetailActivity class

---
+++ app/src/main/java/io/github/ilya_lebedev/worldmeal/ui/detail/MealRecipeDetailActivity.java
@@ -0,0 +1,13 @@
+package io.github.ilya_lebedev.worldmeal.ui.detail;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MealRecipeDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meal_recipe_detail);
+    }
+}

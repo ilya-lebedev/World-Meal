10c200d965d5a9ef0841d7b877454c9e31c87784
Ilya Lebedev <ilya.al.lebedev@gmail.com>
2018-06-29 10:17:18 +0300

    feat: Add MealListActivity class

---
+++ app/src/main/java/io/github/ilya_lebedev/worldmeal/ui/list/MealListActivity.java
@@ -0,0 +1,13 @@
+package io.github.ilya_lebedev.worldmeal.ui.list;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MealListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meal_list);
+    }
+}

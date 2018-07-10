8b423be2e6d29ebdeebb067c4a09509479a51965
Ilya Lebedev <ilya.al.lebedev@gmail.com>
2018-07-10 10:15:09 +0300

    feat: Add implementation of the CategoryEntry

--- app/src/main/java/io/github/ilya_lebedev/worldmeal/data/database/CategoryEntry.java
+++ app/src/main/java/io/github/ilya_lebedev/worldmeal/data/database/CategoryEntry.java
@@ -16,5 +16,44 @@
 
 package io.github.ilya_lebedev.worldmeal.data.database;
 
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+/**
+ * CategoryEntry
+ */
+@Entity(tableName = "category")
 public class CategoryEntry {
+
+    @PrimaryKey
+    private int id;
+    private String name;
+
+    /**
+     * This constructor is used by Room to create CategoryEntries and by MealDbJsonParser.
+     *
+     * @param id Category id
+     * @param name Category name
+     */
+    public CategoryEntry(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }

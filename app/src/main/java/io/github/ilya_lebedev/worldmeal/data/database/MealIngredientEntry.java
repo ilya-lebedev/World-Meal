484581f3a5f3464722b3d812825d1c2df139c4f4
Ilya Lebedev <ilya.al.lebedev@gmail.com>
2018-07-11 09:47:02 +0300

    feat: Add implementation of the MealIngredientEntry

--- app/src/main/java/io/github/ilya_lebedev/worldmeal/data/database/MealIngredientEntry.java
+++ app/src/main/java/io/github/ilya_lebedev/worldmeal/data/database/MealIngredientEntry.java
@@ -16,5 +16,92 @@
 
 package io.github.ilya_lebedev.worldmeal.data.database;
 
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+/**
+ * MealIngredientEntry
+ */
+@Entity(tableName = "meal_ingredient")
 public class MealIngredientEntry {
+
+    @PrimaryKey(autoGenerate = true)
+    private long id;
+    private int indexNumber;
+    private String name;
+    private String measure;
+    private long mealId;
+
+    /**
+     * This constructor is used by Room to create MealIngredientEntries
+     *
+     * @param id Meal ingredient id
+     * @param indexNumber Meal ingredient index number
+     * @param name Meal ingredient name
+     * @param measure Meal ingredient measure
+     * @param mealId Meal ingredient meal id
+     */
+    public MealIngredientEntry(long id, int indexNumber, String name, String measure, long mealId) {
+        this.id = id;
+        this.indexNumber = indexNumber;
+        this.name = name;
+        this.measure = measure;
+        this.mealId = mealId;
+    }
+
+    /**
+     * This constructor is used by MealDbJsonParser.
+     *
+     * @param indexNumber Meal ingredient index number
+     * @param name Meal ingredient name
+     * @param measure Meal ingredient measure
+     * @param mealId Meal ingredient meal id
+     */
+    public MealIngredientEntry(int indexNumber, String name, String measure, long mealId) {
+        this.indexNumber = indexNumber;
+        this.name = name;
+        this.measure = measure;
+        this.mealId = mealId;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getIndexNumber() {
+        return indexNumber;
+    }
+
+    public void setIndexNumber(int indexNumber) {
+        this.indexNumber = indexNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMeasure() {
+        return measure;
+    }
+
+    public void setMeasure(String measure) {
+        this.measure = measure;
+    }
+
+    public long getMealId() {
+        return mealId;
+    }
+
+    public void setMealId(long mealId) {
+        this.mealId = mealId;
+    }
+
 }

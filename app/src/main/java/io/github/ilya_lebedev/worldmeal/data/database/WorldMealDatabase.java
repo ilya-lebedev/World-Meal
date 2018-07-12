f7c673e0d8983f5059c7bb51389f024c7b4c9103
Ilya Lebedev <ilya.al.lebedev@gmail.com>
2018-07-12 16:37:13 +0300

    feat: Add implementation of the WorldMealDatabase

--- app/src/main/java/io/github/ilya_lebedev/worldmeal/data/database/WorldMealDatabase.java
+++ app/src/main/java/io/github/ilya_lebedev/worldmeal/data/database/WorldMealDatabase.java
@@ -16,5 +16,55 @@
 
 package io.github.ilya_lebedev.worldmeal.data.database;
 
-public class WorldMealDatabase {
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+/**
+ * WorldMealDatabase
+ */
+@Database(entities =
+        {
+                AreaEntry.class,
+                AreaListMealEntry.class,
+                CategoryEntry.class,
+                CategoryListMealEntry.class,
+                IngredientEntry.class,
+                IngredientListMealEntry.class,
+                MealEntry.class,
+                MealIngredientEntry.class
+        },
+        version = 1)
+public abstract class WorldMealDatabase extends RoomDatabase {
+
+    private static final String DATABASE_NAME = "world_meal";
+
+    // For Singleton instantiation
+    private static final Object LOCK = new Object();
+    private static volatile WorldMealDatabase sInstance;
+
+    // DAOs
+    public abstract AreaDao areaDao();
+    public abstract AreaListMealDao areaListMealDao();
+    public abstract CategoryDao categoryDao();
+    public abstract CategoryListMealDao categoryListMealDao();
+    public abstract IngredientDao ingredientDao();
+    public abstract IngredientListMealDao ingredientListMealDao();
+    public abstract MealDao mealDao();
+    public abstract MealIngredientDao mealIngredientDao();
+
+    public static WorldMealDatabase getInstance(Context context) {
+        if (sInstance == null) {
+            synchronized (LOCK) {
+                if (sInstance == null) {
+                    sInstance = Room.databaseBuilder(context.getApplicationContext(),
+                            WorldMealDatabase.class, DATABASE_NAME).build();
+                }
+            }
+        }
+
+        return sInstance;
+    }
+
 }

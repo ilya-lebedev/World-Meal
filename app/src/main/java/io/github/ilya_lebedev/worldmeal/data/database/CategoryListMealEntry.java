5ac4562978e86401e551f2a100062adcdc0d406d
Ilya Lebedev <ilya.al.lebedev@gmail.com>
2018-07-12 10:17:36 +0300

    feat: Add implementation of the CategoryListMealEntry

--- app/src/main/java/io/github/ilya_lebedev/worldmeal/data/database/CategoryListMealEntry.java
+++ app/src/main/java/io/github/ilya_lebedev/worldmeal/data/database/CategoryListMealEntry.java
@@ -16,5 +16,65 @@
 
 package io.github.ilya_lebedev.worldmeal.data.database;
 
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+/**
+ * CategoryListMealEntry
+ */
+@Entity(tableName = "category_list_meal")
 public class CategoryListMealEntry {
+
+    @PrimaryKey
+    private long id;
+    private String name;
+    private String thumbnail;
+    private String category;
+
+    /**
+     * This constructor is used by Room to create AreaListMealEntry and by MealDbJsonParser.
+     *
+     * @param id List meal id
+     * @param name List meal name
+     * @param thumbnail List meal thumbnail
+     * @param category List meal category
+     */
+    public CategoryListMealEntry(long id, String name, String thumbnail, String category) {
+        this.id = id;
+        this.name = name;
+        this.thumbnail = thumbnail;
+        this.category = category;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(String thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
 }

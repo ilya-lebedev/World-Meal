b940154a848947706bf43b607c4b5fc3bec4ed7b
Ilya Lebedev <ilya.al.lebedev@gmail.com>
2018-07-16 15:35:37 +0300

    feat: Implement area list parse functionality

--- app/src/main/java/io/github/ilya_lebedev/worldmeal/data/network/MealDbJsonParser.java
+++ app/src/main/java/io/github/ilya_lebedev/worldmeal/data/network/MealDbJsonParser.java
@@ -16,5 +16,50 @@
 
 package io.github.ilya_lebedev.worldmeal.data.network;
 
+import android.support.annotation.Nullable;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import io.github.ilya_lebedev.worldmeal.data.database.AreaEntry;
+import io.github.ilya_lebedev.worldmeal.data.network.response.AreaListResponse;
+
 public class MealDbJsonParser {
+
+    private static final String MDB_MEALS = "meals";
+
+    private static final String MDB_STR_AREA = "strArea";
+
+    @Nullable
+    AreaListResponse parseAreaList(final String areaListJsonStr) throws JSONException {
+        JSONObject areaListJson = new JSONObject(areaListJsonStr);
+
+        AreaEntry[] areaEntries = areaListFromJson(areaListJson);
+
+        return new AreaListResponse(areaEntries);
+    }
+
+    private static AreaEntry[] areaListFromJson(final JSONObject areaListJson) throws JSONException {
+        JSONArray areaJsonArray = areaListJson.getJSONArray(MDB_MEALS);
+
+        AreaEntry[] areaEntries = new AreaEntry[areaJsonArray.length()];
+
+        for (int i = 0; i < areaJsonArray.length(); i++) {
+            JSONObject areaJson = areaJsonArray.getJSONObject(i);
+
+            AreaEntry area = areaEntryFromJson(areaJson);
+
+            areaEntries[i] = area;
+        }
+
+        return areaEntries;
+    }
+
+    private static AreaEntry areaEntryFromJson(final JSONObject areaJson) throws JSONException {
+        String areaName = areaJson.getString(MDB_STR_AREA);
+
+        return new AreaEntry(areaName);
+    }
+
 }

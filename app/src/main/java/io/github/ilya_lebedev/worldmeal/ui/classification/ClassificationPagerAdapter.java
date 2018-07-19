fee237fdbe17ca6600c49e10f203c3fe94cbffc2
Ilya Lebedev <ilya.al.lebedev@gmail.com>
2018-07-19 09:50:24 +0300

    feat: Add ClassificationPagerAdapter

---
+++ app/src/main/java/io/github/ilya_lebedev/worldmeal/ui/classification/ClassificationPagerAdapter.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2018 Ilya Lebedev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.ilya_lebedev.worldmeal.ui.classification;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import io.github.ilya_lebedev.worldmeal.ui.classification.area.AreaListFragment;
+
+/**
+ * ClassificationPagerAdapter
+ */
+public class ClassificationPagerAdapter extends FragmentPagerAdapter {
+
+    private static final int FRAGMENT_COUNT = 1;
+
+    public ClassificationPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return AreaListFragment.newInstance();
+            default:
+                throw new IllegalArgumentException("There is no item for position " + position);
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return FRAGMENT_COUNT;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "Area";
+            default:
+                throw new IllegalArgumentException("There is no title for position " + position);
+        }
+    }
+
+}

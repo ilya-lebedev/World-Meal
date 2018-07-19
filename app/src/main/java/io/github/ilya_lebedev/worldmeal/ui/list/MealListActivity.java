5bb7b9f6215d3d0cd950338c06ebe3c054b173ee
Ilya Lebedev <ilya.al.lebedev@gmail.com>
2018-07-19 19:51:34 +0300

    fix: Set necessary import

--- app/src/main/java/io/github/ilya_lebedev/worldmeal/ui/list/MealListActivity.java
+++ app/src/main/java/io/github/ilya_lebedev/worldmeal/ui/list/MealListActivity.java
@@ -3,6 +3,8 @@ package io.github.ilya_lebedev.worldmeal.ui.list;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import io.github.ilya_lebedev.worldmeal.R;
+
 public class MealListActivity extends AppCompatActivity {
 
     @Override
@@ -10,4 +12,5 @@ public class MealListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meal_list);
     }
+
 }

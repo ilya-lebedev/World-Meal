fa4ff32a20e5ce785fae7e421d39f87c1915184a
Ilya Lebedev <ilya.al.lebedev@gmail.com>
2018-07-20 14:07:54 +0300

    fix: Set necessary import

--- app/src/main/java/io/github/ilya_lebedev/worldmeal/ui/detail/MealRecipeDetailActivity.java
+++ app/src/main/java/io/github/ilya_lebedev/worldmeal/ui/detail/MealRecipeDetailActivity.java
@@ -3,6 +3,8 @@ package io.github.ilya_lebedev.worldmeal.ui.detail;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import io.github.ilya_lebedev.worldmeal.R;
+
 public class MealRecipeDetailActivity extends AppCompatActivity {
 
     @Override
